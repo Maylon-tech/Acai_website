bc95e444062863cd51ddc292b81524b931d22134
Maylon-tech <minatokiroisen77@gmail.com>
2023-09-14 17:27:41 +0900

    some features changes

--- js/main.js
+++ js/main.js
@@ -1,30 +1,23 @@
 const containerCards = document.querySelectorAll('.cards-container')
 const card = document.querySelectorAll('.card')
 const buttontabs = document.querySelectorAll('.buttons-tabs')
-const tabs = document.querySelectorAll('.btn-tab')
-
 
 const filterProducts = (e) => {
     document.querySelector(".active").classList.remove("active")
 
     e.target.classList.add("active")
 
-
-    console.log(card)
     containerCards.forEach(card => {
         card.classList.add("hide")
+
+        if(card.dataset.name === e.target.dataset.name || e.target.dataset.name === "all") {
+            card.classList.remove("hide")
+        }
     })
-
-
-    if(card.dataset.name === e.target.dataset.name) {
-        card.classList.remove("hide")
-    }
-
+    console.log(card)
 }
 
 buttontabs.forEach(button => button.addEventListener("click", filterProducts))
-
-
 
 // LOGIN Modal
 
